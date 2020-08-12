e4d249d8603218acc6489c2782c8029b4e81b205
agiletkiewicz <a.letkiewicz@gmail.com>
2020-08-12 00:58:11 -0700

    Basic user login flow

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,4 +6,15 @@ class SessionsController < ApplicationController
         redirect_to '/'
     end
 
+    def new
+        @user = User.new
+    end
+
+    def create 
+        @user = User.find_by(username: params[:user][:username])
+        session[:user_id] = @user.id
+
+        redirect_to user_path(@user)
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,7 @@ Rails.application.routes.draw do
 
   root 'static#home'
   get '/logout' => 'sessions#destroy'
+  get '/signin' => 'sessions#new'
+  post '/signin' => 'sessions#create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
