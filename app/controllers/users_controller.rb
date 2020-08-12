48b8da1e54fed40fa2ac4a781490a82670e67a5a
agiletkiewicz <a.letkiewicz@gmail.com>
2020-08-12 00:18:53 -0700

    Add basic user signup flow

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,23 @@
 class UsersController < ApplicationController
+
+    def new 
+        @user = User.new
+    end
+
+    def create 
+        @user = User.create(user_params)
+        session[:user_id] = @user.id
+
+        redirect_to user_path(@user)
+    end
+
+    def show 
+    end
+
+    private 
+
+    def user_params 
+        params.require(:user).permit(:name, :username)
+    end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,7 @@ class User < ApplicationRecord
     has_many :goals 
     has_many :tasks, through: :goals
     has_many :notes, through: :goals 
+
+    # has_secure_password
     
 end
