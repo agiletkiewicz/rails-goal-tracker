085dcc19a39749c68656607827799cc83a803b5a
agiletkiewicz <a.letkiewicz@gmail.com>
2020-08-14 10:44:21 -0700

    Order tasks on Goal show page

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -7,5 +7,10 @@ class Goal < ApplicationRecord
 
     scope :completed, -> { where(complete: true) }
     scope :not_completed, -> { where(complete: false) }
+
+    def ordered_open_tasks
+        open_tasks = self.tasks.not_completed
+        sorted = open_tasks.sort_by {|task| task.by_when}
+    end
     
 end
