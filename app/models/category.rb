2dc250f1f7aedb5f48c83cd056f7445c502680e9
agiletkiewicz <a.letkiewicz@gmail.com>
2020-08-18 02:52:18 -0700

    Add category snapshot to user show page.

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,9 @@
 class Category < ApplicationRecord
+    has_many :goals 
+    has_many :users, through: :goals
+
+    def goal_count(user)
+        self.goals.where(user_id: user.id).count
+    end
+
 end
