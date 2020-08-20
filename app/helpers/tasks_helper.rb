c3ce50ab1389266feed6e41a1af7e1f308e0bfa9
agiletkiewicz <a.letkiewicz@gmail.com>
2020-08-19 20:02:19 -0700

    Refactor html on user show page.

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,2 +1,7 @@
 module TasksHelper
+
+    def overdue(task) 
+        task.by_when.to_date < Time.current.to_date
+    end 
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,13 @@
 class User < ApplicationRecord
     has_many :goals 
     has_many :tasks, through: :goals
-
     has_many :notes, through: :goals 
-
     has_many :categories, through: :goals
 
     has_secure_password
 
     validates :name, :username, presence: true
+
 
     def goal_count(category_id)
         self.goals.where(category_id: category_id).count
