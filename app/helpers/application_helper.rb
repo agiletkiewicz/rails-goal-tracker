ee8771215db187e5411a11947c90cb3481e0d55a
agiletkiewicz <a.letkiewicz@gmail.com>
2020-08-21 00:56:25 -0700

    Fix task update bug.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,11 @@
 module ApplicationHelper
 
-    def deadline(goal)
-        goal.by_when.strftime("%A, %b %d")
+    def deadline(goal_task)
+        goal_task.by_when.strftime("%A, %b %d")
     end
 
-    def day_completed(goal)
-        goal.completed_date.strftime("%b %e %Y")
+    def day_completed(goal_task)
+        goal_task.completed_date.strftime("%b %e %Y")
     end
 
 end
