5ee0444cbc41abadfc2907b7117422478a546996
agiletkiewicz <a.letkiewicz@gmail.com>
2020-08-25 21:02:05 -0700

    Fix create account bug.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
         if @user.save
             session[:user_id] = @user.id
 
-            redirect_to user_path(@user)
+            redirect_to '/'
         else
             render :new
         end
