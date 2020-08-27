fcb4f3496df8c8c91c49855837d14cf60ddd786c
agiletkiewicz <a.letkiewicz@gmail.com>
2020-08-26 18:05:45 -0700

    Configure development db.

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_08_21_181858) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "categories", force: :cascade do |t|
     t.string "name"
   end
