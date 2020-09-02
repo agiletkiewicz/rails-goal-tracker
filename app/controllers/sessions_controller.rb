97396af24e0d2e6d55467f56dab3b016de926007
agiletkiewicz <a.letkiewicz@gmail.com>
2020-09-02 00:45:49 -0400

    Set meta tag for image.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,6 +8,7 @@ class SessionsController < ApplicationController
 
     def new
         @user = User.new
+        set_meta_tags image_src: "https://images.pexels.com/photos/733857/pexels-photo-733857.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
         render layout: "welcome"
     end
 
